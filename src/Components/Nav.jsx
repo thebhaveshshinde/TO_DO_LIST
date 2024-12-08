d2a5762bbaff36d23349d38f4484a5d3e9f9de47
thebhaveshshinde23 <thebhaveshshinde@gmail.com>
2024-12-08 13:52:50 +0530

    Notification Number  Added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -2,19 +2,25 @@ import { useState } from "react"
 import { NavLink } from "react-router-dom"
 import { useContext } from "react"
 import { MyContext } from "./MyContext"
+import { MyContext3 } from "./MyContext3"
+import { MyContext2 } from "./MyContext2"
+import { MyContext4 } from "./MyContext4"
 
 
 function Nav() {
     // const [isopen, setisopen] = useState(false)
     const { isopen , setisopen } = useContext(MyContext)
+    const {Delete} = useContext(MyContext3)
+    const {Tasks} = useContext(MyContext2)
+    const {Completed}= useContext(MyContext4)
     return (
         <>
 
             <nav className="hidden w-screen sm:flex sm:justify-center sm:items-center sm:text-center sm:mt-0 sm:bg-slate-600 sm:h-20 sm:fixed sm:top-0 sm:p-2 sm:z-50">
                 <NavLink className="hidden p-3 text-xl italic text-slate-50 sm:flex active:text-slate-800" to="/">Create Task</NavLink>
-                <NavLink className="hidden p-3 text-xl italic text-slate-50 sm:flex active:text-slate-800" to="/All">All Task</NavLink>
-                <NavLink className="hidden p-3 text-xl italic text-slate-50 sm:flex active:text-slate-800" to="/Complete">Completed Task</NavLink>
-                <NavLink className="hidden p-3 text-xl italic text-slate-50 sm:flex active:text-slate-800" to="/Delete">Deleted Task</NavLink>
+                <NavLink className="hidden p-3 text-xl italic text-slate-50 sm:flex active:text-slate-800" to="/All">All Task<strong className="text-xl text-red-950 non-italic">({Tasks.length})</strong></NavLink>
+                <NavLink className="hidden p-3 text-xl italic text-slate-50 sm:flex active:text-slate-800" to="/Complete">Completed Task<strong className="text-xl text-red-950 non-italic">({Completed.length})</strong></NavLink>
+                <NavLink className="hidden p-3 text-xl italic text-slate-50 sm:flex active:text-slate-800" to="/Delete">Deleted Task<strong className="text-xl text-red-950 non-italic">({Delete.length})</strong></NavLink>
                
             </nav>
             <div >
@@ -25,9 +31,9 @@ function Nav() {
                 <div className="fixed flex flex-col items-center w-screen h-screen text-center bg-red-950 justify-evenly sm:hidden z-80">
 
                     <NavLink className="text-2xl text-slate-50 active:text-purple-800 active:text-3xl" to={"/"} onClick={()=>setisopen(false)}>Create Task</NavLink>
-                    <NavLink className="text-2xl text-slate-50 active:text-purple-800 active:text-3xl" to={"/All"} onClick={()=>setisopen(false)}>All Tasks</NavLink>
-                    <NavLink className="text-2xl text-slate-50 active:text-purple-800 active:text-3xl" to={"/Complete"} onClick={()=>setisopen(false)}>Completed Tasks</NavLink>
-                    <NavLink className="text-2xl text-slate-50 active:text-purple-800 active:text-3xl" to={"/Delete"} onClick={()=>setisopen(false)}>Deleted Tasks</NavLink>
+                    <NavLink className="text-2xl text-slate-50 active:text-purple-800 active:text-3xl" to={"/All"} onClick={()=>setisopen(false)}>All Tasks<strong className="text-xl text-blue-500 non-italic ">({Tasks.length})</strong></NavLink>
+                    <NavLink className="text-2xl text-slate-50 active:text-purple-800 active:text-3xl" to={"/Complete"} onClick={()=>setisopen(false)}>Completed Tasks<strong className="text-xl text-blue-500 non-italic ">({Completed.length})</strong></NavLink>
+                    <NavLink className="text-2xl text-slate-50 active:text-purple-800 active:text-3xl" to={"/Delete"} onClick={()=>setisopen(false)}>Deleted Tasks<strong className="text-xl text-blue-500 non-italic ">({Delete.length})</strong></NavLink>
                     
                 </div>
             )}
